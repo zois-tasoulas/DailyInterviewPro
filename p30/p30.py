2ea2ba098d0106476046304e074fe713c71b7099
Zois Tasoulas <zoistas@hotmail.com>
2019-11-02 00:38:34 -0500

    Add problem 30

--- p30/p30.py
+++ p30/p30.py
@@ -1,17 +1,29 @@
-def createHeap(heap, numOfElements):
-    for i in range(k - 1, 0, -1):    #heapify only the first half of the elements
-        heapify(i, heap)
+def swap(index1, index2, array):
+    c = array[index1]
+    array[index1] = array[index2]
+    array[index2] = c
 
-def heapify(index, heap):            #fill out this one
-    if heap[index] > heap[] and heap[] > heap[]:
-        swap()
-        heapify(, heap)
-    elif heap[index] > heap[]:
-        swap()
-        heapify(, heap)
-    elif heap[index] > heap[]:
-        swap()
-        heapify(, heap)
+def createHeap(heap, numOfElements):
+    for i in range(numOfElements // 2 - 1, -1, -1):
+        heapify(i, numOfElements, heap)
+
+def heapify(index, numOfElements, heap):
+    if index > numOfElements // 2 - 1:
+        pass
+    else:
+        if (index + 1) * 2 < numOfElements :
+            if heap[index] > heap[(index + 1) * 2] and heap[(index + 1) * 2] > heap[(index + 1) * 2 - 1]:
+                swap(index, (index + 1) * 2 - 1, heap)
+                heapify((index + 1) * 2 - 1, numOfElements, heap)
+            elif heap[index] > heap[(index + 1) * 2]:
+                swap(index, (index + 1) * 2, heap)
+                heapify((index + 1) * 2, numOfElements, heap)
+            elif heap[index] > heap[(index + 1) * 2 - 1]:
+                swap(index, (index + 1) * 2 - 1, heap)
+                heapify((index +1) * 2 - 1, numOfElements, heap)
+        elif heap[index] > heap[(index + 1) * 2 - 1]:
+            swap(index, (index + 1) * 2 - 1, heap)
+            heapify((index +1) * 2 - 1, numOfElements, heap)
 
 class Solution:
     def kLargestElement(self, lst, k):
@@ -20,11 +32,11 @@ class Solution:
         for i in range(k, len(lst)):
             if lst[i] > minHeap[0]:
                 minHeap[0] = lst[i]
-                heapify(0, minHeap)
+                heapify(0, k, minHeap)
 
         return minHeap[0]
 
 if __name__ == '__main__':
-    lst = [3, 5, 2, 4, 6, 8]
-    k = 3
+    lst = [5, 8, 8, 15, 5, 3, 9, 7, 9, 11, 2, 13]
+    k = 4
     print(Solution().kLargestElement(lst, k))
